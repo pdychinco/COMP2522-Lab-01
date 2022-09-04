e12ba20f77cf77f43cce4a72fed55a1fb65a5172
pdychinco <prdychinco@gmail.com>
2022-09-03 21:46:17 -0700

    Edited file to follow proper indentation protocol and added Javadoc for explanation of the class.

--- src/main/java/comp2522/Main.java
+++ src/main/java/comp2522/Main.java
@@ -1,7 +1,10 @@
 package comp2522;
 
+/**
+ * This is the main function that prints out a statement to the terminal.
+ * */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("My favorite sport is volleyball!");
-    }
+  public static void main(String[] args) {
+    System.out.println("My favorite sport is volleyball!");
+  }
 }
